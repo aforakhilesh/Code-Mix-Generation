7488c319bea728a04f0d6fb4f67fb8821e1a0a1f
AardG <AardG@github.com>
2021-04-25 14:07:19 +0530

    structure changes

--- indermediate_evaluation/data/CodeMixcorpora/makecsv.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import csv
-sents = pd.read_csv('train_clean.txt', header=None, delimiter='\n', error_bad_lines=False, quoting=csv.QUOTE_NONE)
-sents.columns = ["Sentence"]
-
-sents.to_csv('train.csv', index=None)
